5b7c951af070817f91d4761409db5bedecb50255
Marcelina Lima <marcelina.batista@hotmail.com>
2023-06-29 18:04:50 -0300

    Alteraçoes dos nomes dos arquivos

--- aula_6/def_dados.py
+++
@@ -1,9 +0,0 @@
-#função dados
-#idade=None, quando o parametro é none é opcional nao vai receber nada 
-def dados(nome, idade=None):
-    if idade is not None:
-     return('nome: {}\nidade: {} anos'.format(nome,idade))
-    else:
-        return('nome:{}\nidade não informada'.format(nome))
-#chamamos a função dados      
-print(dados('Marcelina','40'))      
--- aula_6/def_teste.py
+++
@@ -1,5 +0,0 @@
-def teste(arg, *args):
-    print('argumento normal: {}'.format(arg))
-    for arg in args:
-        print('outro argumento: {}'.format(arg))
-teste("python","É",  "melhor que", "java")        
