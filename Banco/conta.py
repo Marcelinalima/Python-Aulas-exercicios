fd08a4e7096a7fadd1c3e4056dbc0c8bf692c2ed
Marcelina Lima <marcelina.batista@hotmail.com>
2023-06-30 15:55:20 -0300

    Alteraçoes Banco

---
+++ Banco/conta.py
@@ -0,0 +1,29 @@
+from cliente import Cliente
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print('Inicializando a conta')
+        self.numero = numero
+        self.titular = Cliente.nome
+        self.saldo = saldo
+        self.limite = limite
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        if(self.saldo < valor):
+            return False
+        else :
+            self.saldo -= valor
+            return True
+
+    def extrato(self):
+        print("numero: {} \ntitular: {} \nsaldo: {}".format(self.numero, self.titular, self.saldo))
+
+    def transferir(self, destino, valor):
+        retirada = self.sacar(valor)  
+        if(retirada == False):
+           return False
+        else:
+            destino.depositar(valor)
+            return True
